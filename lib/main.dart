9b461d3361be977b2c1f93afc89f7fb3d182524c
Usama <epiicuree@gmail.com>
2023-11-24 20:38:48 +0500

    Add logged In using Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:chat_app/models/firebase_helper.dart';
+import 'package:chat_app/models/user_model.dart';
+import 'package:chat_app/pages/home_page.dart';
 import 'package:chat_app/pages/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -8,7 +12,14 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+  User? currentUser = FirebaseAuth.instance.currentUser;
+  if (currentUser != null) {
+    UserModel userModel =
+        await FirebaseHelper.getUserModelById(currentUser.uid);
+    MyAppLoggedIn(userModel: userModel, user: currentUser);
+  } else {
+    runApp(const MainApp());
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -19,6 +30,24 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
+    );
+  }
+}
+
+class MyAppLoggedIn extends StatelessWidget {
+  final UserModel userModel;
+  final User user;
+  const MyAppLoggedIn({
+    super.key,
+    required this.userModel,
+    required this.user,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(userModel: userModel, user: user),
     );
   }
 }
---
+++ lib/models/firebase_helper.dart
@@ -0,0 +1,12 @@
+import 'package:chat_app/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseHelper {
+  static Future<UserModel> getUserModelById(String uid) async {
+    UserModel? userModel;
+    DocumentSnapshot snapshot =
+        await FirebaseFirestore.instance.collection("users").doc(uid).get();
+    userModel = UserModel.fromMap(snapshot.data() as Map<String, dynamic>);
+      return userModel;
+  }
+}
