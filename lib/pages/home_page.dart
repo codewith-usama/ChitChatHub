03245fc8223f9df0d3639eca91b57848def256bd
Usama <epiicuree@gmail.com>
2023-12-01 16:35:14 +0500

    Add user msg tile on home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
+import 'package:chat_app/models/chat_room_model.dart';
+import 'package:chat_app/models/firebase_helper.dart';
 import 'package:chat_app/models/user_model.dart';
+import 'package:chat_app/pages/chat_room_page.dart';
 import 'package:chat_app/pages/search_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -31,8 +35,80 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: const SafeArea(
-        child: SizedBox.shrink(),
+      body: SafeArea(
+        child: Container(
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection("chartooms")
+                .where("participants.${widget.userModel.uid}", isEqualTo: true)
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      ChatRoomModel chatRoomModel = ChatRoomModel.fromMap(
+                          snapshot.data!.docs[index].data()
+                              as Map<String, dynamic>);
+                      Map<String, dynamic>? participants =
+                          chatRoomModel.participants;
+                      List<String> participantsKeys =
+                          participants!.keys.toList();
+
+                      participantsKeys.remove(widget.userModel.uid);
+
+                      return FutureBuilder(
+                        future: FirebaseHelper.getUserModelById(
+                            participantsKeys[0]),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<UserModel?> userData) {
+                          if (userData.connectionState ==
+                              ConnectionState.done) {
+                            if (userData.data != null) {
+                              return ListTile(
+                                onTap: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => ChatRoomPage(
+                                        chatRoomModel: chatRoomModel,
+                                        targetUser: userData as UserModel,
+                                        userModel: widget.userModel,
+                                        user: widget.user,
+                                      ),
+                                    ),
+                                  );
+                                },
+                                leading: CircleAvatar(
+                                  backgroundColor: Colors.grey.shade300,
+                                  backgroundImage: NetworkImage(
+                                      userData.data!.profilePicUrl!),
+                                ),
+                                title: Text(userData.data!.fullName!),
+                                subtitle: Text(chatRoomModel.lastMessage!),
+                              );
+                            } else {
+                              return const SizedBox.shrink();
+                            }
+                          } else {
+                            return const SizedBox.shrink();
+                          }
+                        },
+                      );
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.hasError.toString()));
+                } else {
+                  return const Center(child: Text('No chats'));
+                }
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
